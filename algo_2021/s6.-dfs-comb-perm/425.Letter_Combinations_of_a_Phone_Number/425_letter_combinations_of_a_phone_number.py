11d19c3743ff1bafd7eb87bf0fc66a73a7d27aae
Jerry Wu <iamjerrywu@gmail.com>
2021-03-15 00:08:27 +0800

    add problem 425

---
+++ algo_2021/s6.-dfs-comb-perm/425.Letter_Combinations_of_a_Phone_Number/425_letter_combinations_of_a_phone_number.py
@@ -0,0 +1,32 @@
+KEYBOARD = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+}
+
+class Solution:
+    """
+    @param digits: A digital string
+    @return: all posible letter combinations
+    """
+    def letterCombinations(self, digits):
+        # write your code here
+        if not digits:
+            return []
+        res = []
+        self.dfs(digits, 0, [], res)
+        return res
+    
+    def dfs(self, digits, index, chars, res):
+        if index == len(digits):
+            res.append(''.join(chars))
+            return 
+        for letter in KEYBOARD[digits[index]]:
+            chars.append(letter)
+            self.dfs(digits, index + 1, chars, res)
+            chars.pop()
